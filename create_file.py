027170aa7c9cdde23065ef574077a69aed4d7922
briecheeze <143107686+briecheeze@users.noreply.github.com>
2024-03-11 12:38:45 -0600

    binary_test

--- create_file.py
+++ create_file.py
@@ -8,4 +8,11 @@ format = Struct(
     "pixels" / Array(this.width * this.height, Byte),
 )
 
-print(format.build(dict(width=3,height=2,pixels=[7,8,9,11,12,13])))
+binary_content= format.build(dict(width=3,height=2,pixels=[7,8,9,11,12,13]))
+
+# Open a file in binary write mode ('wb' mode)
+with open('binary_file.bin', 'wb') as f:
+    # Write binary content to the file
+    f.write(binary_content)
+
+print("Binary content has been written to binary_file.bin")
